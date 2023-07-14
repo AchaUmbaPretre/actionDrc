97c6881c9c6d8a6b42b324922b824283282f5634
AchaUmbaPretre <achandambi@gmail.com>
2023-07-14 14:56:02 +0100

    commit

--- src/components/formation/Formation.jsx
+++ src/components/formation/Formation.jsx
@@ -1,5 +1,5 @@
 import './formation.scss'
-
+import formation from './../../img/formation.png'
 const Formation = () => {
   return (
     <>
@@ -11,10 +11,18 @@ const Formation = () => {
                         <h1 className="client-sous-title">moteur de développement</h1>
                         <p className="apropos-barre"><span className="apropos-moov"></span></p>
                     </div>
+                    <div className="formation-text">
+                        <p className="formation-desc">
+                        Nous croyons que la formation est la clé pour fournir des services de qualité
+                        supérieure. C'est pourquoi nous offrons des formations de mise à niveau pour
+                        notre personnel, afin de garantir que nous délivrons les meilleurs standards
+                        avec les meilleures pratiques du moment.
+                        </p>
+                    </div>
                 </div>
 
                 <div className="formation-right">
-
+                    <img src={formation} alt="" className="formation-img" />
                 </div>
             </div>
         </div>
