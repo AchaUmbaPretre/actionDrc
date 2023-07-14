4e63fe3e8a330cca0105f880f14f89040a933466
AchaUmbaPretre <achandambi@gmail.com>
2023-07-14 14:10:13 +0100

    commit

--- src/components/equipe/Equipe.jsx
+++ src/components/equipe/Equipe.jsx
@@ -47,12 +47,64 @@ const Equipe = () => {
 
                         <div className="equipe-row">
                             <div className="equipe-top">
-                                <img src={nana} alt="" className="equipe-img" />
+                                <img src={faustin} alt="" className="equipe-img" />
                             </div>
                             <div className="equipe-bottom">
                                 <div className="equipe-bottom-rows">
-                                    <h2 className="equipe-name">Nana MANDA</h2>
-                                    <h3 className="equipe-h3">General Manager</h3>
+                                    <h2 className="equipe-name">Faustin BONGALI</h2>
+                                    <h3 className="equipe-h3">DAF</h3>
+                                    <div className="equipe-rx">
+                                        <div className="equipe-rows-rx">
+                                            <FacebookOutlined/>
+                                        </div>
+                                        <div className="equipe-rows-rx">
+                                            <Twitter/>
+                                        </div>
+                                        <div className="equipe-rows-rx">
+                                            <LinkedIn/>
+                                        </div>
+                                        <div className="equipe-rows-rx">
+                                            <Instagram/>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="equipe-row">
+                            <div className="equipe-top">
+                                <img src={nesto} alt="" className="equipe-img" />
+                            </div>
+                            <div className="equipe-bottom">
+                                <div className="equipe-bottom-rows">
+                                    <h2 className="equipe-name">Nestor MAKWELA</h2>
+                                    <h3 className="equipe-h3">Chef d'Agence Kinshasa</h3>
+                                    <div className="equipe-rx">
+                                        <div className="equipe-rows-rx">
+                                            <FacebookOutlined/>
+                                        </div>
+                                        <div className="equipe-rows-rx">
+                                            <Twitter/>
+                                        </div>
+                                        <div className="equipe-rows-rx">
+                                            <LinkedIn/>
+                                        </div>
+                                        <div className="equipe-rows-rx">
+                                            <Instagram/>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="equipe-row">
+                            <div className="equipe-top">
+                                <img src={maxim} alt="" className="equipe-img" />
+                            </div>
+                            <div className="equipe-bottom">
+                                <div className="equipe-bottom-rows">
+                                    <h2 className="equipe-name">Maxim NGUZ E.</h2>
+                                    <h3 className="equipe-h3">Chef d'Agence Katanga</h3>
                                     <div className="equipe-rx">
                                         <div className="equipe-rows-rx">
                                             <FacebookOutlined/>
@@ -77,8 +129,8 @@ const Equipe = () => {
                             </div>
                             <div className="equipe-bottom">
                                 <div className="equipe-bottom-rows">
-                                    <h2 className="equipe-name">Nana MANDA</h2>
-                                    <h3 className="equipe-h3">General Manager</h3>
+                                    <h2 className="equipe-name">Patrick MONGA M.</h2>
+                                    <h3 className="equipe-h3">Financial Manager Katanga</h3>
                                     <div className="equipe-rx">
                                         <div className="equipe-rows-rx">
                                             <FacebookOutlined/>
@@ -99,12 +151,12 @@ const Equipe = () => {
 
                         <div className="equipe-row">
                             <div className="equipe-top">
-                                <img src={nana} alt="" className="equipe-img" />
+                                <img src={jeremi} alt="" className="equipe-img" />
                             </div>
                             <div className="equipe-bottom">
                                 <div className="equipe-bottom-rows">
-                                    <h2 className="equipe-name">Nana MANDA</h2>
-                                    <h3 className="equipe-h3">General Manager</h3>
+                                    <h2 className="equipe-name">Jeremie WAYWAY K.</h2>
+                                    <h3 className="equipe-h3">Supervisor</h3>
                                     <div className="equipe-rx">
                                         <div className="equipe-rows-rx">
                                             <FacebookOutlined/>
@@ -122,59 +174,6 @@ const Equipe = () => {
                                 </div>
                             </div>
                         </div>
-
-                        <div className="equipe-row">
-                            <div className="equipe-top">
-                                <img src={nana} alt="" className="equipe-img" />
-                            </div>
-                            <div className="equipe-bottom">
-                                <div className="equipe-bottom-rows">
-                                    <h2 className="equipe-name">Nana MANDA</h2>
-                                    <h3 className="equipe-h3">General Manager</h3>
-                                    <div className="equipe-rx">
-                                        <div className="equipe-rows-rx">
-                                            <FacebookOutlined/>
-                                        </div>
-                                        <div className="equipe-rows-rx">
-                                            <Twitter/>
-                                        </div>
-                                        <div className="equipe-rows-rx">
-                                            <LinkedIn/>
-                                        </div>
-                                        <div className="equipe-rows-rx">
-                                            <Instagram/>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="equipe-row">
-                            <div className="equipe-top">
-                                <img src={nana} alt="" className="equipe-img" />
-                            </div>
-                            <div className="equipe-bottom">
-                                <div className="equipe-bottom-rows">
-                                    <h2 className="equipe-name">Nana MANDA</h2>
-                                    <h3 className="equipe-h3">General Manager</h3>
-                                    <div className="equipe-rx">
-                                        <div className="equipe-rows-rx">
-                                            <FacebookOutlined/>
-                                        </div>
-                                        <div className="equipe-rows-rx">
-                                            <Twitter/>
-                                        </div>
-                                        <div className="equipe-rows-rx">
-                                            <LinkedIn/>
-                                        </div>
-                                        <div className="equipe-rows-rx">
-                                            <Instagram/>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
                     </div>
                 </div>
             </div>
