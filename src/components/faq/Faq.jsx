ce6b605c5e565929bc00c8be6cec497c5aa121c4
AchaUmbaPretre <achandambi@gmail.com>
2023-07-14 16:50:50 +0100

    commit

--- src/components/faq/Faq.jsx
+++ src/components/faq/Faq.jsx
@@ -71,7 +71,7 @@ const Faq = () => {
 
                         <div class="question-row">
                             <div class="visible-pannel">
-                                <h2 class="question-h2">Qu'est-ce qu'une agence de placement de personnel et en quoi consiste votre activité,</h2>
+                                <h2 class="question-h2">Qu'est-ce qu'une agence de placement de personnel et en quoi consiste votre activité ?</h2>
                                 <ExpandMoreIcon className='icons-plus'/>
                             </div>
                             <div class="toggle-pannel">
@@ -85,14 +85,57 @@ const Faq = () => {
 
                         <div class="question-row">
                             <div class="visible-pannel">
-                                <h2 class="question-h2">Comment pouvez-vous aider les petites entreprises qui n'ont pas d'expérience en matière de transactions internationales ?</h2>
+                                <h2 class="question-h2">Quels sont les avantages de faire appel à une agence de placement de personnel pour le recrutement de mon entreprise ?</h2>
                                 <ExpandMoreIcon className='icons-plus'/>
                             </div>
                             <div class="toggle-pannel">
                                 <p class="question-desc">
-                                Chez Actions SARL, nous travaillons avec des entreprises de toutes tailles, y compris des petites entreprises qui n'ont pas
-                                d'expérience en matière de transactions internationales. Nous sommes là pour vous guider à chaque étape du processus, en
-                                fournissant des conseils et des services adaptés à vos besoins spécifiques
+                                Le recrutement de personnel peut être un processus long et complexe, impliquant la publication d'annonces, le tri des candidatures, la sélection
+                                des candidats et la réalisation d'entretiens. Faire appel à une agence de placement de personnel telle que Actions SARL peut vous aider à économiser
+                                du temps et de l'argent tout en vous offrant une plus grande flexibilité dans la gestion de votre personnel. De plus nous formons gratuitement votre personnel.
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="question-row">
+                            <div class="visible-pannel">
+                                <h2 class="question-h2">Comment ça se passe pour le personnel de maison ?</h2>
+                                <ExpandMoreIcon className='icons-plus'/>
+                            </div>
+                            <div class="toggle-pannel">
+                                <p class="question-desc">
+                                Chez Actions SARL, nous avons une véritable expertise en la matière. Nous formons des candidats
+                                souvent à partir de rien pour les mettre au diapason selon les meilleurs standards internationaux.
+                                Ainsi vous pourrez trouver chez nous des majordomes, des nounous selon différents critères, des femmes
+                                de ménage, des chauffeurs, des gardiens de maison.
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="question-row">
+                            <div class="visible-pannel">
+                                <h2 class="question-h2">Pourquoi Actions DRC pour le personnel de maison ?</h2>
+                                <ExpandMoreIcon className='icons-plus'/>
+                            </div>
+                            <div class="toggle-pannel">
+                                <p class="question-desc">
+                                En République Démocratique du Congo, de nombreuses jeunes filles et des mères de famille travaillent comme
+                                femmes de ménage. Si certaines d'entre elles ont pu trouver ce travail grâce à une agence, la plupart sont
+                                recrutées sur base des relations interpersonnelles, amicales ou de bouche à oreille. Dans bien des cas, elles
+                                travaillent pendant des années sans contrat et sans le moindre.
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="question-row">
+                            <div class="visible-pannel">
+                                <h2 class="question-h2">Quels types de services offrez-vous en matière de placement de personnel ?</h2>
+                                <ExpandMoreIcon className='icons-plus'/>
+                            </div>
+                            <div class="toggle-pannel">
+                                <p class="question-desc">
+                                Chez Actions SARL, nous offrons une gamme complète de services de placement de personnel, y compris la sélection de candidats, la vérification des antécédents et des références,
+                                 l'évaluation des compétences, la formation, la gestion des contrats de travail, la gestion de la paie et des avantages sociaux, et la gestion de la conformité réglementaire
                                 </p>
                             </div>
                         </div>
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -9,15 +9,15 @@ const Topbar = () => {
                 <div className="topbar-left">
                     <div className="topbar-left-icons">
                         <LocationOn className='topbar-icons'/>
-                        <span className="topbar-adresse">6 tabora, Kinshasa gombe, Rdc</span>
+                        <span className="topbar-adresse">25 avenue de l'OUA Quartier GB Commune de Ngaliema</span>
                     </div>
                     <div className="topbar-left-icons topNum">
                         <Phone className='topbar-icons'/>
-                        <span className="topbar-adresse">+243810836943</span>
+                        <span className="topbar-adresse">+243 819 444 440</span>
                     </div>
                     <div className="topbar-left-icons">
                         <Mail className='topbar-icons'/>
-                        <span className="topbar-adresse">hello@243technologies.com</span>
+                        <span className="topbar-adresse">Info@actionsdrc.Com</span>
                     </div>
                 </div>
                 <div className="topbar-right">
