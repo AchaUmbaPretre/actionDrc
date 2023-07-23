1d3019d0fdfb85ec92cfaea9186f7d247a63ba62
AchaUmbaPretre <achandambi@gmail.com>
2023-07-23 13:59:33 +0100

    commit

--- src/components/specialité/Specialite.jsx
+++ src/components/specialité/Specialite.jsx
@@ -19,41 +19,41 @@ const Specialite = () => {
             </div>
 
             <div className="specialite-container">
-              <div className="specialite-row">
+              <div className="specialite-row" data-aos="zoom-in-up">
                 <img src={cuisinier} alt="" className="specialite-img" />
                 <p className="specialite-desc">Cuisinier</p>
               </div>
 
-              <div className="specialite-row">
+              <div className="specialite-row" data-aos="zoom-in-up">
                 <img src={nounou} alt="" className="specialite-img" />
                 <p className="specialite-desc">Nounou à domicile</p>
               </div>
 
               <div className="specialite-row">
-                <img src={chauffeur} alt="" className="specialite-img" />
+                <img src={chauffeur} alt="" className="specialite-img" data-aos="zoom-in-up" />
                 <p className="specialite-desc">Chauffeur</p>
               </div>
 
-              <div className="specialite-row">
+              <div className="specialite-row" data-aos="zoom-in-up">
                 <img src={gouvernante} alt="" className="specialite-img" />
                 <p className="specialite-desc">Gouvernante</p>
               </div>
 
-              <div className="specialite-row">
+              <div className="specialite-row" data-aos="zoom-in-up">
                 <img src={lavadier} alt="" className="specialite-img" />
                 <p className="specialite-desc">Lavadier</p>
               </div>
 
-              <div className="specialite-row">
+              <div className="specialite-row" data-aos="zoom-in-up">
                 <img src={majordome} alt="" className="specialite-img" />
                 <p className="specialite-desc">Majordome</p>
               </div>
 
-              <div className="specialite-row">
+              <div className="specialite-row" data-aos="zoom-in-up">
                 <img src={femme} alt="" className="specialite-img" />
                 <p className="specialite-desc">Femme de ménage</p>
               </div>
-              <div className="specialite-row">
+              <div className="specialite-row" data-aos="zoom-in-up">
                 <img src={pediatrie} alt="" className="specialite-img" />
                 <p className="specialite-desc">Puéricultrice</p>
               </div>
