a2fcf39283cbf04dad4277d13ca708f27b935fcf
AchaUmbaPretre <achandambi@gmail.com>
2023-08-16 11:29:28 +0100

    commit

--- src/components/slider/slider2/Sliders.jsx
+++ src/components/slider/slider2/Sliders.jsx
@@ -52,7 +52,7 @@ export default function App() {
         <SwiperSlide>
           <div className="slider-row3">
             <div className="slider-wrapper">
-              <h1 className="sliders-h1" data-aos="fade-right">Ensemble c’est possible</h1>
+              <h1 className="sliders-h1" data-aos="fade-right">Bienvenue chez Actions DRC !</h1>
               <p className="slider-desc" data-aos="fade-left">Bienvenue sur le portail de ACTIONS SARL, votre Agence en Douane et partenaire de confiance pour le placement de personnel. Nous sommes basés à Kinshasa et nous avons une présence bien établie dans tout le pays, notamment dans les zones minières à forte activité économique.</p>
               <div className="sliders-row">
                 <a href="#apropos" className="slider-btn" data-aos="fade-right">Savoir plus <PlayCircleIcon className='icons-btn'/></a>
